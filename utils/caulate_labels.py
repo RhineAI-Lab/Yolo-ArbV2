9ad81a35a484608c339b1a91d8ea5fbeed0688fd
HRan2004 <2125764918@qq.com>
2021-11-30 12:09:53 +0800

    First commit

---
+++ utils/caulate_labels.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+inp = np.array([717,58,846,332,266,598,137,337])
+size = np.array([1000,711])
+inp=inp.astype(np.float64)
+
+bt = np.zeros([4])
+box = np.zeros([4])
+bt[0] = np.min(inp[::2])
+bt[2] = np.max(inp[::2])
+bt[1] = np.min(inp[1::2])
+bt[3] = np.max(inp[1::2])
+box[0] = (bt[0] + bt[2]) / 2
+box[1] = (bt[1] + bt[3]) / 2
+box[2] = bt[2] - bt[0]
+box[3] = bt[3] - bt[1]
+
+box[::2] /= size[0]
+box[1::2] /= size[1]
+inp[::2] /= size[0]
+inp[1::2] /= size[1]
+
+strbox = ""
+for i in box:
+    strbox += " "+format(i, '.4f')
+strpoly = ""
+for i in inp:
+    strpoly += " "+format(i, '.4f')
+print("0"+strbox)
+print("0"+strpoly)
